18f0e7259c38295eb9d3407696c6494414c437c9
Thassya <thayowisky@gmail.com>
2020-03-12 15:48:13 +0100

    2 files modified

--- app/template-form/template-form.component.ts
+++ app/template-form/template-form.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from "@angular/core";
 import { FormGroup, FormControl, FormBuilder } from "@angular/forms";
-import { Http } from '@angular/http';
-import 'rxjs/add/operator/map';
+import { Http } from "@angular/http";
+import "rxjs/add/operator/map";
 
 @Component({
   selector: "app-template-form",
@@ -16,30 +16,66 @@ export class TemplateFormComponent implements OnInit {
 
   onSubmit(form) {
     console.log(form);
-
+    this.http.post('enderecoServer/formUsuario', JSON.stringify(form.value))
+    .map(res=>res)
+    .subscribe(dados => console.log(dados));
   }
 
   constructor(private http: Http) {}
 
   ngOnInit() {}
-  verificaValidTouched(campo){
+  verificaValidTouched(campo) {
     return !campo.valid && campo.touched;
   }
-  aplicaCssErro(campo){
+  aplicaCssErro(campo) {
     return {
-      'has-error': this.verificaValidTouched(campo), 
-      'has-feedback': this.verificaValidTouched(campo)
-    }
+      "has-error": this.verificaValidTouched(campo),
+      "has-feedback": this.verificaValidTouched(campo)
+    };
   }
 
-  consultaCEP(cep){
-    cep=cep.replace(/\D/g, '');
-    if(cep != ""){
+  consultaCEP(cep, form) {
+    cep = cep.replace(/\D/g, "");
+    if (cep != "") {
       var validacep = /^[0-9]{8}$/;
-       if(validacep.test(cep)) { //
-          this.http.get(`https://viacep.com.br/ws/${cep}/json/`).map(dados=>dados.json())
-          .subscribe((dados)=>{console.log(dados)});
-       }
-    } 
+      if (validacep.test(cep)) {
+        
+        this.limpaFormulario(form);
+
+        this.http
+          .get(`https://viacep.com.br/ws/${cep}/json/`)
+          .map(dados => dados.json())
+          .subscribe(dados => {
+            this.populaDadosForm(dados, form);
+          });
+      }
+    }
   }
+  populaDadosForm(dados, formulario) {
+
+    formulario.form.patchValue({
+      endereco: {
+        cep: dados.cep,
+        complemento: dados.complemento,
+        rua: dados.logradouro,
+        bairro: dados.bairro,
+        cidade: dados.localidade,
+        estado: dados.uf
+      }
+    })
+  }
+
+  limpaFormulario(formulario){
+      formulario.form.patchValue({
+      endereco: {
+        complemento: null,
+        rua: null,
+        bairro: null,
+        cidade: null,
+        estado: null
+      }
+    })
+  }
+  
+
 }
